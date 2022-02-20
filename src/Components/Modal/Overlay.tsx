d9cfd7c407208b95350b2420a48d47fb90ff50f5
Bradley Martin <bradley.r.martin@me.com>
2022-02-20 18:08:23 +1100

    update

--- src/Components/Modal/Overlay.tsx
+++ src/Components/Modal/Overlay.tsx
@@ -5,5 +5,5 @@ export interface OverlayProps {
 }
 
 export const Overlay = ({ children }: OverlayProps): JSX.Element => {
-    return <div className="absolute inset-0 bg-black bg-opacity-25">{children}</div>;
+    return <div className="absolute inset-0 bg-black bg-opacity-25 z-50">{children}</div>;
 };
