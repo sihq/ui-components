cb12735e6fdc3b8ccca7d5535757b5ff213e777f
Bradley Martin <bradley.r.martin@me.com>
2022-03-05 18:03:28 +1100

    update

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ export * from './Components/Modal';
 export * from './Components/Button';
 export * from './Components/Form/Field';
 
+import { ReactiveControllerContext } from './Contexts/ReactiveControllerContext';
 import { withController } from './Providers/withController';
 import { withReactive } from './Providers/withReactive';
 
 export const Reactive = withReactive(({ children }): any => children);
-
 export const Controller = withController;
+export const Context = ReactiveControllerContext;
