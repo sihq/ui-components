523cae71359c987a10a9f50cd65360e7506739fa
Bradley Martin <bradley.r.martin@me.com>
2022-05-15 21:52:11 +1000

    update

--- src/Components/Form/Inputs/Toggle.tsx
+++ src/Components/Form/Inputs/Toggle.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode, useContext } from 'react';
 
 import Conditional from '../../Conditional';
-import { ControllerContext } from '@sihq/reactive';
+import { FieldContext } from '../../../Contexts';
 import InlineErrors from '../shared/InlineErrors';
 import Label from '../shared/Label';
 import PrivacyBarrier from '../shared/PrivacyBarrier';
@@ -20,8 +20,9 @@ interface ToggleProperties extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 export default (props: ToggleProperties): JSX.Element => {
-    const { bind = (): object => ({}) } = useContext(ControllerContext);
-    const { id, name, label, defer = true, append, prepend, disabled, placeholder } = props;
+    const context = useContext(FieldContext);
+
+    const { id, name, label, append, prepend, disabled, placeholder } = props;
 
     return (
         <>
@@ -29,15 +30,17 @@ export default (props: ToggleProperties): JSX.Element => {
                 data-input="checkbox"
                 className="flex relative flex-col text-gray-600 focus-within:text-blue-500 -ml-1 -mr-2 rounded p-1 pr-2 hover:text-blue-500 cursor-pointer text-xs group"
             >
-                <span data-input="toggle" className="flex justify-start items-center relative">
+                <label data-input="toggle" className="flex justify-start items-center relative">
                     {prepend}
                     <div className="relative flex-shrink-0 rounded-full w-7 h-4 transition duration-200 ease-linear">
                         <input
                             type="checkbox"
                             className="opacity-0 absolute inset-0 w-full h-full z-40"
+                            ref={context.ref}
                             id={`${id ?? name ?? label}`}
                             {...{ name, disabled, placeholder }}
-                            {...bind({ defer, name })}
+                            checked={context.value}
+                            onChange={() => context.onChange(context.value ? false : true)}
                         />
                         <span className="checked opacity-0 bg-green-500 absolute z-10 left-0 w-full h-full rounded-full transition duration-100 ease-linear"></span>
                         <span className="bg-gray-400 absolute left-0 w-full h-full rounded-full"></span>
@@ -49,7 +52,7 @@ export default (props: ToggleProperties): JSX.Element => {
                         </div>
                     </Conditional>
                     {append}
-                </span>
+                </label>
                 <PrivacyBarrier />
                 <InlineErrors />
             </span>
